1f96917cb0c29c1b212b6473cd4d1fae0b5ad4b6
arrowill74 <arrowill74@gmail.com>
2018-04-10 11:55:23 +0800

    torchseat

---
+++ Assets/Playerclosed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playerclosed : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject sand;
+	public CollisionListScript PlayerSensor;
+	private Rigidbody rigidBody;
+	void Start () {
+		rigidBody = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PlayerSensor.CollisionObjects.Count > 0) {
+			sand.SetActive(false);
+		}
+	}
+}
